a5f6c5b6d28a320d36450897f10fdf77fcb3f5f6
mayesha6 <soumyrhmn234@gmail.com>
2026-09-26 16:03:05 +0600

    validation fix

--- src/app/modules/review/review.validation.ts
+++ src/app/modules/review/review.validation.ts
@@ -1,18 +1,22 @@
 import { z } from "zod";
 
 const createReviewZodSchema = z.object({
-  body: z.object({
-    booking: z.string(),
-    rating: z.number().min(1).max(5),
-    comment: z.string().optional(),
-  }),
+
+
+  booking: z.string(),
+  rating: z.number().min(1).max(5),
+  comment: z.string().optional(),
+
+
 });
 
 const updateReviewZodSchema = z.object({
-  body: z.object({
-    rating: z.number().min(1).max(5).optional(),
-    comment: z.string().optional(),
-  }),
+
+
+  rating: z.number().min(1).max(5).optional(),
+  comment: z.string().optional(),
+
+
 });
 
 export const ReviewValidations = {
